4984c59c97e2af7713866ad3f463bbaf7c41cc12
Kevin Bumgarner <kgbumgarner@gmail.com>
2022-01-11 23:51:39 -0600

    Style changes

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,9 +1,19 @@
 <main>
-    <h1>Binerator</h1>
-    <p>Click "Generate" to reveal a random Linkedin profile background!</p>
+    <h1>B<span>in</span>erator</h1>
+    <p>Click <span>"Generate"</span> to reveal a random Linkedin profile background!</p>
 </main>
 
 
 <style>
+    main {
+      display: flex; 
+      flex-direction: column;
+      margin: auto;
+      align-items: center;
+      margin: 40px; 
+    }
 
+    span {
+        color: #00a0dc;
+    }
 </style>
